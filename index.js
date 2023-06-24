8c2c557a5750b085b4439ab73d9e8424ef172511
Ammar Talpur <83033514+ammartalpur@users.noreply.github.com>
2023-06-24 14:13:25 +0500

    added vercel

--- index.js
+++ index.js
@@ -25,7 +25,32 @@ const shayariSchema = mongoose.Schema({
 
 const Shayari = mongoose.model("Shayari", shayariSchema);
 
+app.get('/shayari', (req, res) => {
+    // Get the count of all users
+    Shayari.countDocuments().exec().then(function (count) {
 
+        // Get a random entry
+        var random = Math.floor(Math.random() * count)
+        // console.log(random);
+        // Again query all users but only fetch one offset by our random #
+        Shayari.findOne().skip(random).exec().then(
+            function (result) {
+                // Tada! random user
+                res.send(result)
+            })
+    })
+})
+app.post('/shayari', (req, res) => {
+    const newShayari = new Shayari({
+        writer: req.body.writer,
+        shayari: req.body.shayari
+    })
+    newShayari.save().then((result) => {
+        res.send("Successfully added shayari to database")
+    }).catch((err) => {
+        res.send(err)
+    });
+})
 db.on('open', () => {
     console.log("Successfully connected to database!");
     app.listen(port, hostname, () => {
